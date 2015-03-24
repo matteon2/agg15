fdcb3db66a8264d5797aa528d8ad91209b6f84a6
Matt <mk448@INF900076.ad.susx.ac.uk>
2015-03-24 14:24:09 +0000

    v0 - empty commitment

---
+++ ant/src/ant/Ant.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ant;
+
+/**
+ *
+ * @author mk448
+ */
+public class Ant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
