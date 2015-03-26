998f7e476903f0f04e76fdd225cb36d0e982110a
jdc29 <joyce.d.corsel@gmail.com>
2015-03-26 12:52:25 +0000

    Further SWING implementation

--- ant/src/ant/gameGUI.java
+++ ant/src/ant/gameGUI.java
@@ -6,14 +6,14 @@ import javax.swing.*;
  *
  * @author joycecorsel
  */
-public class AntGameGUI extends JFrame {
+public class gameGUI extends JFrame {
     int WINDOW_WIDTH = 500;
     int WINDOW_HEIGHT = 500;
 
     /**
      * Creates new form gameGUI
      */
-    public AntGameGUI() {
+    public gameGUI() {
         initComponents();
         setTitle("Ant Game");
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
@@ -49,34 +49,11 @@ public class AntGameGUI extends JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(AntGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(AntGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(AntGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(AntGameGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new AntGameGUI().setVisible(true);
+                new gameGUI().setVisible(true);
             }
         });
     }
