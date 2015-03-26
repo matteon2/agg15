470f57017a324d3137f748cc6c09fd474e365158
Matt <mk448@INF900076.ad.susx.ac.uk>
2015-03-26 13:30:21 +0000

    Reverted main to empty (using separate file for the GUI, see previous commit)

--- ant/src/ant/Ant.java
+++ ant/src/ant/Ant.java
@@ -4,70 +4,18 @@
  */
 package ant;
 
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-
 /**
  *
  * @author mk448
  */
-public class Ant extends JFrame{
-        public Ant() {
-            initUI();
-        }
+public class Ant {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        EventQueue.invokeLater(new Runnable() {
-
-            @Override
-            public void run() {
-                Ant game = new Ant();
-                game.setVisible(true);
-            }
-        });
-    }
-
-    private void initUI() {
-        createMenuBar();
-
-        setTitle("Simple menu");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-
-    private void createMenuBar() {
-        JMenuBar menubar = new JMenuBar();
-        ImageIcon icon = new ImageIcon("exit.png");
-
-        JMenu file = new JMenu("File");
-        file.setMnemonic(KeyEvent.VK_F);
-
-        JMenuItem eMenuItem = new JMenuItem("Exit", icon);
-        eMenuItem.setMnemonic(KeyEvent.VK_E);
-        eMenuItem.setToolTipText("Exit application");
-        eMenuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent event) {
-                System.exit(0);
-            }
-        });
-
-        file.add(eMenuItem);
-        menubar.add(file);
-
-        setJMenuBar(menubar);
+        // TODO code application logic here
+        //
+        //
     }
 }
