69e7d3ed0fb072bd85710dace5f56b73e9dcec07
sl410 <sl410@INF900816.ad.susx.ac.uk>
2015-04-01 11:22:49 +0100

    added some shit to cell class

--- ant/src/ant/Cell.java
+++ ant/src/ant/Cell.java
@@ -9,15 +9,18 @@ public class Cell {
     
     private int x;
     private int y;
+    private char contents;
     
-    public Cell(int x, int y){
+    public Cell(int x, int y, char contents){
         this.x = x;
         this.y = y;
+        this.contents = contents;
     }
     
     public int getX(){return x;}
     public int getY(){return y;}
+    public char getContents(){return contents;}
     public void setX(int x){this.x = x;}
     public void setY(int y){this.y = y;}
-    
+    public void setContents(char contents){this.contents = contents;}
 }
