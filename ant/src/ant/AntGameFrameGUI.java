3296c9e7797712dc4370731fcc49eb7e611abe34
jdc29 <joyce.d.corsel@gmail.com>
2015-04-06 18:57:42 +0100

    Functionality added. All buttons in the main menu direct to related JPanel when clicked. 'Quit' button closes the program. 'Back' button directs to main menu.

--- ant/src/ant/AntGameFrameGUI.java
+++ ant/src/ant/AntGameFrameGUI.java
@@ -1,18 +1,14 @@
 package ant;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.event.*;
+import javax.swing.*;
+
 
 /**
  *
  * @author joycecorsel
  */
+
 public class AntGameFrameGUI extends JFrame {
     private JPanel mMenu, tournamentMode, duelMode, settingsDisplay, gameDisplay;
     private JButton duel, tourn, quit, settings, 
@@ -30,26 +26,113 @@ public class AntGameFrameGUI extends JFrame {
     {
         mMenu = new JPanel();
         duel = new JButton("Duel");
+        duel.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(duelMode); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         tourn = new JButton("Tournament");
+        tourn.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(tournamentMode); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         quit = new JButton("Quit");
+        quit.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
         settings = new JButton("Settings");
+        settings.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(settingsDisplay); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         
         duelMode = new JPanel();
+        
         uploadBrainDuel = new JButton("Upload Ant Brain");
+        
         uploadWorldDuel = new JButton("Upload Ant World");
+               
         randomWorldDuel = new JCheckBox("Random World");
+        
         playDuel = new JButton("Play");
+        playDuel.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(gameDisplay); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
+        
         backDuel = new JButton("Back");
+        backDuel.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(mMenu); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         
         tournamentMode = new JPanel();
         uploadBrainTournament = new JButton("Upload Ant Brain");
+                
         uploadWorldTournament = new JButton("Upload Ant World");
-        randomWorldTournament = new JCheckBox("Random World");
-        playTournament = new JButton("Play");
-        backTournament = new JButton("Back");
         
+        randomWorldTournament = new JCheckBox("Random World");
+        
+        playTournament = new JButton("Play");
+        playTournament.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(gameDisplay); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
+        
+        backTournament = new JButton("Back");
+        backTournament.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(mMenu); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         settingsDisplay = new JPanel();
+        
         backSettings = new JButton("Back");
+        backSettings.addActionListener(new addButtonListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                getContentPane().removeAll();
+                getContentPane().add(mMenu); //Adding to content pane, not to AntGameFrameGUI
+                repaint();
+                printAll(getGraphics());
+            }
+        });
         
         gameDisplay = new JPanel();
     }
@@ -87,6 +170,12 @@ public class AntGameFrameGUI extends JFrame {
         //frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    private static abstract class addButtonListener implements ActionListener {
+
+        public addButtonListener() {
+        }
     }
     
 }
